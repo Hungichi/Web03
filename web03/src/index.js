5c4d5d38d0a32553cd1fb3fcd84126f4967f772e
Hungichi <conanfa3@gmail.com>
2024-08-20 16:38:30 +0700

    category

--- web03/src/index.js
+++ web03/src/index.js
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
-    <Body/>
-    {/* <Categories/> */}
+    {/* <Body/> */}
+    <Categories/>
     <Footer/>
 
 
--- web03/src/pages/Categories/category.js
+++ web03/src/pages/Categories/category.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {FaChevronDown} from 'react-icons/fa';
 
 function Categories() {
     return (
        <div className='d-flex'>
-        <div style={{ backgroundColor:"#D5D5D5",width:"32%", height:"200px", margin:"20px"}} ></div>
+        <div style={{ backgroundColor:"#D5D5D5",width:"32%", height:"500px", margin:"20px"}} className='d-flex flex-column' > 
+            <div>
+               <div style={{}} className="d-flex ">
+                  <div class="p-2 flex-grow-1 ">Product Catalog</div>
+                  <button><FaChevronDown/></button>
+               </div>   
+            </div>
+        </div>
         <div style={{width:"70%", margin:"20px", height:"200px" }}  className='bg-secondary'></div>
         
        </div>
