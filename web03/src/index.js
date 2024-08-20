4e3d6b9a03231b52f93b15b4ecf684c961a1f7bb
MinhMan888 <minh.man0102002@gmail.com>
2024-08-20 16:06:29 +0700

    fix

--- web03/src/index.js
+++ web03/src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Index from './src/Auth/signin/Index';
+import SignUp from './src/Auth/signup/SignUp';
+import Footer from './src/page/Footer';
+import Header from './src/page/Header';
+import Profile from './src/Auth/account/Profile';
+import productDetails from './src/page/productDetails';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <Header/>
+    <Profile/> */}
+          <SignUp/>
+    {/* <Index/>
     <App />
+    <Footer/>
+    <productDetails/> */}
   </React.StrictMode>
 );
 
---
+++ web03/src/src/Auth/signin/Index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './SignIn.css';
+const Index = () => {
+  return (
+    <div>
+      <div class='container'> 
+        <h1>Welcome Back !</h1> 
+        <h3>Fps Store</h3> 
+        <h4>_ _ _ _ _ _ _ _ _</h4> 
+      </div> 
+      <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+      </div>
+    <form>
+        <h3>Login</h3>
+        <input type="text" placeholder="UserName" id="username"></input>
+        <input type="password" placeholder="Password" id="password"></input>
+        <div class='remember-forgot'>
+          <div class='remember-me'>
+            <input type='checkbox' id='remember'></input>
+            <label for='remember'>Remember me</label>
+          </div>
+          <div class='forgot'>
+            <a href='#'>Forgot password?</a>
+          </div>
+        </div>
+        <button>Login</button>
+        <div class="social">
+          <h5 class='line-one'>__________</h5> 
+          <h6>Or</h6>
+          <h5 class='line-two'>__________</h5>
+        </div>
+        <a  href='#'><img  src='./gmail.png' className="gmail" alt="logo" /></a>
+        <div class='register'>
+          <span>Don't have an account ?<a href='#'> SignUp</a></span>
+        </div>
+        <div class='term'>
+          <p >Terms & Conditions</p> 
+          <p >Support</p> 
+          <p >Customer Care</p>
+        </div>
+    </form>
+    </div>
+  )
+}
+
+export default Index
