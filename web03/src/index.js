54245916fe1f4f1e3d3959f52cf89b0a71fff6bb
Hungichi <conanfa3@gmail.com>
2024-08-20 16:49:37 +0700

    update

--- web03/src/index.js
+++ web03/src/index.js
@@ -1,21 +1,29 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './pages/Home/header';
 import Body from './pages/Home/body';
 import Footer from './pages/Home/footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Index from './Auth/signin/Index';
+import SignUp from './Auth/SignUp/SignUp';
+import Profile from './Auth/account/Profile';
+import productDetails from './pages/Home/productDetails';
 import Categories from './pages/Categories/category';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
-    {/* <Body/> */}
     <Categories/>
+    {/* <Profile/> */}
+    {/* <SignUp/> */}
+    {/* <Index/>
+    <App /> */}
     <Footer/>
-
-
+    {/* <productDetails/> */}
   </React.StrictMode>
 );
 
