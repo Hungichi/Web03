4d9c771ad5fedbfb5ad38ca07739c2a96b35b795
Nguyen Thao Ngoc <ngocnguyenthao140@gmail.com>
2024-08-24 16:44:26 +0700

    update

--- web03/src/index.js
+++ web03/src/index.js
@@ -14,7 +14,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Index from './Auth/signin/Index';
 import SignUp from './Auth/SignUp/SignUp';
 import Profile from './Auth/account/Profile';
-import productDetails from './pages/Home/productDetails';
+import productDetails from './pages/ProductDetail/productDetails';
 import Categories from './pages/Categories/category';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
